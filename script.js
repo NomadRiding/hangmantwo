510e785454ab745ec3e7b52ab7c3c4b43c2770e6
NomadRiding <nomadriding@gmail.com>
2020-02-11 11:46:35 -0500

    start end or reset barebone structure

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function initNewGame() {
+ // Ask player to start a new game
+}
 
+function endGame() {
+// clear the game, await user responce
+}
+
+function newOrEndGame() {
+// game finished, ask user to end or start a new game.
+}
