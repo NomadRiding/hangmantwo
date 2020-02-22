884019ae963f9173e2e199c833a84310eac6d732
NomadRiding <nomadriding@gmail.com>
2020-02-22 11:16:38 -0500

    start variables

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+let userLife = 6
+let userPick = getElementById();
+let wordBank = ["wolf"];
+let playAgain = true;
+
 function initNewGame() {
  // Ask player to start a new game
+
+
 }
 
 function endGame() {
 // clear the game, await user responce
+
+if (userLife > 0) {
+  userPick
+} else {
+  //restart game
+}
+
 }
 
 function newOrEndGame() {
